76d762c3e4961348ae6cb04b21b988445256b8bf
Chunarev Sergei <chunarev.se.a@gmail.com>
2024-03-04 20:55:56 +0100

    update group

--- src/main/java/com/memorizing/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/memorizing/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.eurekaserver;
+package com.memorizing.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
